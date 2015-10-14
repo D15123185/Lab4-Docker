fbf6bcb233366a97715c8a065efecab6dfea09e6
Ubuntu <ubuntu@ip-172-30-0-37.eu-west-1.compute.internal>
2015-10-14 15:18:41 +0000

    lab 5 finish

--- lab5/my_application/server.py
+++ lab5/my_application/server.py
@@ -2,8 +2,23 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
+def index():
+	return 'Index Page'
+
+@app.route("/hello")
 def hello():
     return "Hello World!"
 
+
+@app.route("/user/<username>")
+def userpaul(username):
+	return 'User %s' % username
+
+@app.route("/post/<port>")
+def post80(port):
+	return 'Post %s' % port
+
+
 if __name__ == "__main__":
-    app.run(host="0.0.0.0")
+    app.run(host="0.0.0.0", port=5000,debug=True)
+
